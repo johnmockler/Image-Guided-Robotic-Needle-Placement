ce20f459d79419b7523dbc2bd4ffbbcbde5dad6d
John Mockler <jmockler94@gmail.com>
2020-06-05 17:15:50 +0200

    added main.cpp

---
+++ src/camera_calibration/src/main.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+
+#include "CameraCalibrationNode.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "camera_calibration");
+
+    CameraCalibrationNode node;
+
+    ROS_INFO("Calibration node initiated");
+
+    ros::spin();
+
+    return EXIT_SUCCESS;
+
+}

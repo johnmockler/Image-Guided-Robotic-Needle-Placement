56d5e3a5d15709739e30e228ddf40dc76bcec3fe
Akshay Shetty <akshay.shetty@tuhh.de>
2020-06-20 21:42:17 +0200

    Cord Publisher included
    coordinates being published using geometry_msgs

--- build/forward_kinematics/catkin_generated/pkg.develspace.context.pc.py
+++ build/forward_kinematics/catkin_generated/pkg.develspace.context.pc.py
@@ -1,7 +1,7 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/rnm/rnm_group_2/src/forward_kinematics/include".split(';') if "/home/rnm/rnm_group_2/src/forward_kinematics/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PROJECT_CATKIN_DEPENDS = "geometry_msgs".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
 PROJECT_NAME = "forward_kinematics"
 PROJECT_SPACE_DIR = "/home/rnm/rnm_group_2/devel"
--- src/forward_kinematics/src/forward_kinematics_node.cpp
+++ src/forward_kinematics/src/forward_kinematics_node.cpp
@@ -8,6 +8,10 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <stdlib.h>
 #include <math.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Point.h>
+
 
 class Kinematics{
   
@@ -24,9 +28,10 @@ private:
  cv::Mat T_6;
  cv::Mat T_7;
  cv::Mat T_F;
- //ros::NodeHandle n;
+ ros::NodeHandle n;
 
- //ros::Publisher EEcord_pub = n.advertise<std_msgs::Float64MultiArray>("/endeffector_Cord", 1000);
+ ros::Publisher EEcord_pub = n.advertise<std_msgs::Float64MultiArray>("/endeffector_Cord", 1000);
+ ros::Publisher test_pub = n.advertise<geometry_msgs::Point>("/tf_publisher", 1000);
 
 public:
 Kinematics()
@@ -78,7 +83,7 @@ void compute(std::vector<float> theta)
 void setCoordinates(std::vector<cv::Mat> transformations)
 {
   std::cout<<"entered setcord"<<std::endl;
-  cv::Point3f coord;
+  std::vector<double> coord;
   cv::Mat finalTrans= cv::Mat::eye(4,4,CV_32F);
   float mat_data1[16]={1,2,3,4,1,2,3,4,1,2,3,4,0,0,0,1};
   cv::Mat temp1 = cv::Mat(4,4,CV_32F,mat_data1);
@@ -91,15 +96,20 @@ void setCoordinates(std::vector<cv::Mat> transformations)
   }
   
   std::cout<<finalTrans<<std::endl;
-  coord.x=0;
-  coord.y=0;
-  coord.z=finalTrans.at<float>(2,3);
-  //std::cout<<coord.x<<""<<coord.y<<""<<coord.z<<std::endl;
+  coord.push_back(finalTrans.at<float>(0,3));
+  coord.push_back(finalTrans.at<float>(1,3));
+  coord.push_back(finalTrans.at<float>(2,3));
 
-  //std_msgs::Float64MultiArray msg;
-        //msg.data.clear();
-        //msg.data.insert(msg.data.end(), coord.begin(), coord.end());
-        //EEcord_pub.publish(msg);
+geometry_msgs::Point newCord;
+newCord.x=25.0;
+newCord.y=10.0;
+newCord.z=12.0;
+test_pub.publish(newCord);
+
+  std_msgs::Float64MultiArray msg;
+        msg.data.clear();
+        msg.data.insert(msg.data.end(), coord.begin(), coord.end());
+        EEcord_pub.publish(msg);
 
 }
 };

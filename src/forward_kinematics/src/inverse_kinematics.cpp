61f82eca65cc94085c8bab5bdda3b14d02a01c42
Akshay Shetty <akshay.shetty@tuhh.de>
2020-07-02 17:38:29 +0200

    Inverse Kinematics completion
    Code test works for test condition

--- src/forward_kinematics/src/inverse_kinematics.cpp
+++ src/forward_kinematics/src/inverse_kinematics.cpp
@@ -12,6 +12,8 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Point.h>
 
+
+
 class InverseKinematics
 {
     private:
@@ -19,17 +21,18 @@ class InverseKinematics
      std::vector<float> d={-0.107,0,-0.384,0,-0.316,0,-0.333};
      std::vector<float> alpha={-M_PI/2,-M_PI/2,M_PI/2,-M_PI/2,-M_PI/2,M_PI/2,0};
      std::vector<float> a={-0.088,0,0.0825,-0.0825,0,0,0};
+     float mat_dat[16]={0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
      float gamma;
      float alph;
      float zeta;
      float epsilon;
      float beta;
      float delta;
+     cv::Mat Rf_47;
      cv::Mat R_07;
      cv::Mat goalPf;
      cv::Mat Tf_0g;
      cv::Mat Tf_01;
-     cv::Mat Tf_1g;
      cv::Mat Tf_02;
      cv::Mat Tf_03;
      cv::Mat Tf_04;
@@ -47,26 +50,61 @@ class InverseKinematics
     double SinT(double theta)
     {
         //std::cout<<"entered Sin"<<std::endl;
-        if(theta > 1.5705)//for angles closer to 90deg
+        float val= sin(theta);
+         if(val<0.0001 && val> -0.001)
+        {
+            return 0.0;
+        }
+        else if(val>0.99)
         {
             return 1.0;
         }
+        else if(val<-0.99)
+        {
+            return -1.0;
+        }
         else
         {
-            return sin(theta);
+            return val;
         }
+        
+        
     }
     float CosT(float theta)
     {
         //std::cout<<"entered COS"<<std::endl;
-        if(theta > 1.5705)//for angles closer to 90deg
+        float val= cos(theta);
+         if(val<0.0001 && val> -0.001)
+        {
+            return 0.0;
+        }
+        else if(val>0.99)
+        {
+            return 1.0;
+        }
+        else if(val<-0.99)
+        {
+            return -1.0;
+        }
+        else
+        {
+            return val;
+        }
+        
+    }
+    float aCos(float val)
+    {
+        float angle;
+        angle=acos(val);
+         if(angle<0.0001 && angle> -0.001)
         {
             return 0.0;
         }
         else
         {
-            return cos(theta);
+            return angle;
         }
+
     }
     float aTan(float a, float b)
     {
@@ -86,28 +124,34 @@ class InverseKinematics
         
 
     }
-    cv::Mat TMatrix(float a, float d, float alpha, float theta)
+    void TMatrix(float *p, float a, float d, float alpha, float theta)
     {
         //std::cout<<"entered Tmat"<<std::endl;
-        cv::Mat T;
-        float mat_data[16]={CosT(theta),-CosT(alpha)*SinT(theta),SinT(alpha)*SinT(theta),a*CosT(theta),  
+        //cv::Mat Tni;
+         float mat[16] ={CosT(theta),-CosT(alpha)*SinT(theta),SinT(alpha)*SinT(theta),a*CosT(theta),  
                             SinT(theta),CosT(alpha)*CosT(theta),-SinT(alpha)*CosT(theta),a*SinT(theta),  
                             0,SinT(alpha),CosT(alpha),d,  
                             0,0,0,1};
-        T= cv::Mat(4,4,CV_32F,mat_data);
-        return T;
+        //Tni= cv::Mat(4,4,CV_32F,mat_dat);
+        for(int i=0;i<16;i++)
+        {
+            p[i]=mat[i];
+        }
+        
+        
     }
 
     cv::Mat TMatrixInv(float a, float d, float alpha, float theta)
     {
         //std::cout<<"entered TMatINv"<<std::endl;
-        cv::Mat T;
-        float mat_data[16]={CosT(theta),SinT(theta),0,-a,  
+        cv::Mat Tin;
+        float matData[16]={CosT(theta),SinT(theta),0,-a,  
                             -CosT(alpha)*SinT(theta),CosT(alpha)*CosT(theta),SinT(alpha),-d*SinT(alpha),  
                             SinT(alpha)*SinT(theta),-SinT(alpha)*CosT(theta),CosT(alpha),-d*CosT(alpha),  
                             0,0,0,1};
-        T= cv::Mat(4,4,CV_32F,mat_data);
-        return T;
+        Tin= cv::Mat(4,4,CV_32F,matData);
+        std::cout<<"Tinv :"<<Tin<<std::endl;
+        return Tin;
     }
     void InitialTransformation(std::vector<float> pos, std::vector<float> ori)
     {
@@ -130,7 +174,6 @@ class InverseKinematics
         R_z3=cv::Mat(3,3,CV_32F,data3);
 
         R_07=R_z1*R_x*R_z3;
-        std::cout<<"R_07 :"<<R_07<<std::endl;
         float data4[16]={R_07.at<float>(0,0),R_07.at<float>(0,1),R_07.at<float>(0,2),pos[0],
                         R_07.at<float>(1,0),R_07.at<float>(1,1),R_07.at<float>(1,2),pos[1],
                         R_07.at<float>(2,0),R_07.at<float>(2,1),R_07.at<float>(2,2),pos[2],
@@ -139,20 +182,31 @@ class InverseKinematics
         goalPf=goalP.inv();
         Tf_0g=goalPf;
         Tf_0g.at<float>(2,3)=Tf_0g.at<float>(2,3) - 0.333;
-        std::cout<<"Tf_0g :"<<Tf_0g<<std::endl;
 
     }
-    cv::Mat CalculateRotationMatrix(float j1, float j2, float j3, float j4)
+    void CalculateRotationMatrix(float j1, float j2, float j3, float j4)
     {
         //std::cout<<"entered CRM"<<std::endl;
-        Tf_01=TMatrix(a[0],d[0], alpha[0], j1);
-        cv::Mat Tf_12=TMatrix(a[1],d[1], alpha[1], j2);
-        cv::Mat Tf_23=TMatrix(a[2],d[2], alpha[2], j3);
-        cv::Mat Tf_34=TMatrix(a[3],d[3], alpha[3], j4);
+        float p[16];
+        TMatrix(&p[0], a[0],d[0], alpha[0], j1);
+        Tf_01= cv::Mat(4,4,CV_32F,p);
+        
+        float p1[16];
+        TMatrix(&p1[0],a[1],d[1], alpha[1], j2);
+        cv::Mat Tf_12=cv::Mat(4,4,CV_32F,p1);
+
+        float p2[16];
+        TMatrix(&p2[0],a[2],d[2], alpha[2], j3);
+        cv::Mat Tf_23=cv::Mat(4,4,CV_32F,p2);
+
+        float p3[16];
+        TMatrix(&p3[0], a[3],d[3], alpha[3], j4);
+        cv::Mat Tf_34=cv::Mat(4,4,CV_32F,p3);
+
         Tf_02=Tf_01*Tf_12;
         Tf_03=Tf_02*Tf_23;
         Tf_04=Tf_03*Tf_34;
-        cv::Mat Rf_04, Rf_07, Rf_47;
+        cv::Mat Rf_04, Rf_07;
         float mat_data[9]={goalPf.at<float>(0,0),goalPf.at<float>(0,1),goalPf.at<float>(0,2),  
                             goalPf.at<float>(1,0),goalPf.at<float>(1,1),goalPf.at<float>(1,2),  
                             goalPf.at<float>(2,0),goalPf.at<float>(2,1),goalPf.at<float>(2,2)};
@@ -164,76 +218,97 @@ class InverseKinematics
         Rf_07=cv::Mat(3,3,CV_32F,mat_data);
         Rf_04=cv::Mat(3,3,CV_32F,mat_data1);
         Rf_47= Rf_04.inv()*Rf_07.inv();
-
-        return Rf_47;
                             
     }
 
-    void ComputeAngles(cv::Mat T, float j_1)
+    void ComputeAngles(float j_1)
     {
         //std::cout<<"entered Compute"<<std::endl;
         float l3,l5,l35;
-        cv::Mat Tf_10=TMatrixInv(a[0],d[0],alpha[0],j_1);
-        Tf_1g= Tf_10 * T;
-        std::cout<<"Transformation Matrix :"<<Tf_1g<<std::endl;
+        cv::Mat Tf_1g;
+        cv::Mat Tf_10;
+        float matData[16]={CosT(j_1),SinT(j_1),0,-a[0],  
+                            -CosT(alpha[0])*SinT(j_1),CosT(alpha[0])*CosT(j_1),SinT(alpha[0]),-d[0]*SinT(alpha[0]),  
+                            SinT(alpha[0])*SinT(j_1),-SinT(alpha[0])*CosT(j_1),CosT(alpha[0]),-d[0]*CosT(alpha[0]),  
+                            0,0,0,1};
+        Tf_10 = cv::Mat(4,4,CV_32F,matData);
+        //std::cout<<"Tf_10 :"<<Tf_10<<std::endl;
+        Tf_1g= Tf_10 * Tf_0g;
+        //std::cout<<"Tf_1g :"<<Tf_1g<<std::endl;
         std::vector<float> pGoalPos;
         pGoalPos.push_back(Tf_1g.at<float>(0,3));
         pGoalPos.push_back(Tf_1g.at<float>(1,3));
         pGoalPos.push_back(Tf_1g.at<float>(2,3));
         gamma=aTan(a[2],-d[2]);
-        alph=aTan(pGoalPos[0],pGoalPos[1]);
+        alph=aTan(pGoalPos[0],-pGoalPos[1]);
         l3=sqrt(pow(d[2],2)+pow(a[2],2));
         l5=sqrt(pow(d[4],2)+pow(a[3],2));
         zeta=aTan(-d[2],a[2]);
         epsilon=aTan(-d[4],-a[3]);
         l35=sqrt(pGoalPos[0]*pGoalPos[0]+pGoalPos[1]*pGoalPos[1]);
-        beta=acos((pow(l3,2)+pow(l35,2)-pow(l5,2))/(2*l3*l35));
-        delta=acos((pow(l3,2)+pow(l5,2)-pow(l35,2))/(2*l3*l5));
-        std::cout<<"alpha :"<<alph<<" beta :"<<beta<<" gamma :"<<gamma<<" zeta :"<<zeta<<" delta :"<<delta<<" epsilon :"<<epsilon<<std::endl;
-        std::cout<<"l3 :"<<l3<<" l5 :"<<l5<<" l_35 :"<<l35<<std::endl; 
+        float num=pow(l3,2)+pow(l35,2)-pow(l5,2);
+        //std::cout<<"num :"<<num<<std::endl;
+        
+        float den=2.0*l3*l35;
+       // std::cout<<"den :"<<den<<std::endl;
+        beta=aCos(num/den);
+        num=pow(l3,2)+pow(l5,2)-pow(l35,2);
+        den= 2.0*l3*l5;
+        delta=aCos(num/den);
+        //std::cout<<"alpha :"<<alph<<" beta :"<<beta<<" gamma :"<<gamma<<" zeta :"<<zeta<<" delta :"<<delta<<" epsilon :"<<epsilon<<std::endl;
+        //std::cout<<"l3 :"<<l3<<" l5 :"<<l5<<" l_35 :"<<l35<<std::endl; 
 
+    }
+    std::vector<float> getAngles(std::vector<float> JAinv)
+    {
+        std::vector<float> angles;
+        for(int i=0;i<7;i++)
+        {
+            angles.push_back(-JAinv[6-i]);
+        }
+        return angles;
     }
     std::vector<float> getInversK(std::vector<float> posVector)
     {
         //std::cout<<"entered getInverseK"<<std::endl;
         float j1,j2,j3,j4,j5,j6,j7;
         std::vector<float> jointAngles;
-        std::vector<float> orientation= {0,M_1_PI,0};
+        std::vector<float> orientation= {0,M_PI,0};
         InitialTransformation(posVector,orientation);
         j1= aTan(-Tf_0g.at<float>(1,3),-Tf_0g.at<float>(0,3));
-        std::cout<<j1<<std::endl;
-        ComputeAngles(Tf_0g,j1);
+
+        ComputeAngles(j1);
         jointAngles.push_back(j1);
         j2=alph-beta-gamma;
         jointAngles.push_back(j2);
         j3=0;
         jointAngles.push_back(j3);
-        j4=2*M_PI-delta-epsilon-zeta;
+        j4=2.0*M_PI-delta-epsilon-zeta;
         jointAngles.push_back(j4);
-        cv::Mat rotMat = CalculateRotationMatrix(j1, j2, j3, j4);
-        std::cout<<"Rotation Matrix is "<<rotMat<<std::endl;
-        j5= aTan(rotMat.at<float>(1,2),rotMat.at<float>(0,3));
+
+        CalculateRotationMatrix(j1, j2, j3, j4);
+        j5= aTan(Rf_47.at<float>(1,2),Rf_47.at<float>(0,3));
         jointAngles.push_back(j5);
-        float temp = sqrt(1- pow(rotMat.at<float>(2,2),2));
-        j6= aTan(temp,rotMat.at<float>(2,2));
+        float temp = sqrt(1- pow(Rf_47.at<float>(2,2),2));
+        j6= aTan(-temp,Rf_47.at<float>(2,2));
         jointAngles.push_back(j6);
-        j7= aTan(rotMat.at<float>(2,1),rotMat.at<float>(2,0));
+        j7= aTan(Rf_47.at<float>(2,1),-Rf_47.at<float>(2,0));
         jointAngles.push_back(j7);
 
-        std::cout<< -j7 <<" "<<-j6<<" "<<-j5<<" "<<-j4<<" "<<-j3<<" "<<-j2<<" "<<-j1<<std::endl;
+        std::vector<float> jointAn= getAngles(jointAngles);
 
-        /*std_msgs::Float64MultiArray msg;
+        std_msgs::Float64MultiArray msg;
         msg.data.clear();
-        msg.data.insert(msg.data.end(), jointAngles.begin(), jointAngles.end());
-        command_pub.publish(msg);*/
-        return jointAngles;
+        msg.data.insert(msg.data.end(), jointAn.begin(), jointAn.end());
+        command_pub.publish(msg);
+        return jointAn;
 
     }
 };
 void jointStateCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 {
     std::cout<<"entered callback"<<std::endl;
-    std::vector<float> cordinates{0.088,0.0,0.168};
+    std::vector<float> cordinates{0.180,0.1,0.618};
     std::vector<float> ja;
 
   /*for (size_t i = 0; i < msg.size(); i++)
@@ -242,6 +317,10 @@ void jointStateCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
   } */
   InverseKinematics inKinObj;
   ja=inKinObj.getInversK(cordinates);
+  for (int i = 0; i < 7; i++)
+  {
+    std::cout<<ja[i]<<std::endl;
+  }
 
 }
 
@@ -250,6 +329,7 @@ int main(int argc, char** argv)
 
   ros::init(argc, argv, "inverse_kinematics");
   ros::NodeHandle nh;
+  InverseKinematics inKinObj;
   ros::Subscriber sub = nh.subscribe("/endeffector_Cord", 1, jointStateCallback);
   ros::spin();
 
